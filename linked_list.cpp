5ac9538a09056ab7270763848a54a9d033996af9
Mr. Zi <md.ziaulhasan00@gmail.com>
2022-08-12 15:26:36 +0600

    operations name added

--- linked_list.cpp
+++ linked_list.cpp
@@ -1,5 +1,35 @@
 #include<bits/stdc++.h>
 using namespace std;
+
+//Linked List Creator Class:
+
+//Insertion at Head:
+
+//Insertion at Tail:
+
+//Insertion at Specific Position: 
+
+//Insertion after Specific Value: 
+
+//Length of a Linear Linked List:
+
+//Search a Specific Value in Unique Valued Linked List:
+
+//Search a Specific Value in Duplicate Enabled Linked List:
+
+//Delation at Head:
+
+//Delation at Tail: 
+
+//Delation at a Specific Position: 
+
+//Delate by Value in Uniqued Valued Linked List: 
+
+//Reverse a Linear Linked List: 
+
+//Doubly Linked List: 
+
+
 int main()
 {
 

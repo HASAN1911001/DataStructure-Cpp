141130f49a3a3313d6d19e285fcf9d71cca21378
Mr. Zi <md.ziaulhasan00@gmail.com>
2022-08-12 19:25:48 +0600

    problem-2 solved

--- Module-20-Practice/2.cpp
+++ Module-20-Practice/2.cpp
@@ -10,3 +10,29 @@ Sample output:
 
 
 --------------------------------*/
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    bool flag = true;
+    cin>>n;
+    for(int i=2; i<=n; i++)
+    {
+        for(int j=2; j<i; j++)
+        {
+            if(i%j == 0)
+            {
+                flag = false;
+                break;
+            }
+        }
+        if(flag)
+        {
+            cout<<i<<" ";
+        }
+        else flag = true;
+    }
+    return 0;
+}
--- Module-20-Practice/3.cpp
+++ Module-20-Practice/3.cpp
@@ -2,5 +2,4 @@
 
 Why is the complexity of binary search O(logN) where N is the size of the list? Explain.
 
-
 --------------------------------*/

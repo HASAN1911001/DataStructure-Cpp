e09f007e6812508f9e38f188ac4621d935f2a02c
Mr. Zi <md.ziaulhasan00@gmail.com>
2022-08-12 15:51:28 +0600

    Array.cpp added

---
+++ Module-20-Practice/Array.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//Take Array Input:
+void inputArray(int array, n)
+{
+    for(int i=0; i<n; i++)
+    {
+        cin<<array[i];
+    }
+}
+
+//Give Array Output:
+void outputArray(int array, int n)
+{
+    for(int i=0; i<n; i++)
+}
+int main()
+{
+
+    return 0;
+}
